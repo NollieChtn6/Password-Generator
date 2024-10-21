ddc18ac130639162de0938be1f5abaae97233ef6
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-10-21 23:11:30 +0200

    implement generator's feature

--- src/components/PasswordGenerator.tsx
+++ src/components/PasswordGenerator.tsx
@@ -1,22 +1,72 @@
-import { Slider } from "primereact/slider";
+import { Slider, type SliderChangeEvent } from "primereact/slider";
 import { Checkbox, type CheckboxChangeEvent } from "primereact/checkbox";
 import { useState } from "react";
 
-export function PasswordGenerator() {
-  const [value, setValue] = useState<number>(12);
+type Password = {
+  nbOfCharacters: number;
+  hasUpperCase: boolean;
+  hasLowerCase: boolean;
+  hasSymbols: boolean;
+  hasNumbers: boolean;
+  hasSpecialCharacters: boolean;
+};
 
-  const [options, setOptions] = useState({
-    includeUppercase: false,
-    includeNumbers: false,
-    includeSpecialChars: false,
+export function PasswordGenerator() {
+  const [passwordOptions, setPasswordOptions] = useState<Password>({
+    nbOfCharacters: 12,
+    hasUpperCase: false,
+    hasLowerCase: false,
+    hasSymbols: false,
+    hasNumbers: false,
+    hasSpecialCharacters: false,
   });
+
+  const [generatedPassword, setGeneratedPassword] = useState<string>("");
 
   const handleCheckBox = (e: CheckboxChangeEvent) => {
     const { name, checked } = e.target;
-    setOptions((prevOptions) => ({
-      ...prevOptions,
+    setPasswordOptions((prev) => ({
+      ...prev,
       [name]: checked,
     }));
+  };
+
+  const handleSlider = (e: SliderChangeEvent) => {
+    const value = e.value as number;
+    setPasswordOptions((prev) => ({
+      ...prev,
+      nbOfCharacters: value,
+    }));
+  };
+
+  // Code snippet: https://www.geeksforgeeks.org/how-to-generate-random-password-in-react/
+
+  const generatePassword = (options: Password) => {
+    const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numberChars = "0123456789";
+    const symbolChars = "!@#$%^&*()_+[]{}|;:,.<>?";
+
+    let str = "abcdefghijklmnopqrstuvwxyz";
+    if (options.hasLowerCase) str += lowercaseChars;
+    if (options.hasUpperCase) str += uppercaseChars;
+    if (options.hasNumbers) str += numberChars;
+    if (options.hasSpecialCharacters) str += symbolChars;
+
+    if (str === "") {
+      str = lowercaseChars;
+    }
+    let newPassword = "";
+    for (let i = 0; i < passwordOptions.nbOfCharacters; i++) {
+      const randomIndex = Math.floor(Math.random() * str.length);
+      newPassword += str[randomIndex];
+    }
+
+    setGeneratedPassword(newPassword);
+  };
+
+  const handleGenerateClick = () => {
+    generatePassword(passwordOptions);
   };
 
   return (
@@ -26,18 +76,23 @@ export function PasswordGenerator() {
       <div className="password-container">
         <label htmlFor="password">Your password</label>
         <div>
-          <input type="text" id="password" value={"my password"} disabled={true} />
+          <input
+            type="text"
+            id="password"
+            value={generatedPassword || "Generate a password!"}
+            disabled={true}
+          />
           <button type="button">Copy</button>
         </div>
       </div>
 
       <div className="slider-container" style={{ position: "relative" }}>
-        <label htmlFor="slider">Number of characters: {value}</label>
+        <label htmlFor="slider">Number of characters: {passwordOptions.nbOfCharacters}</label>
         <Slider
           min={12}
           max={50}
-          value={value}
-          onChange={(e) => setValue(e.value as number)}
+          value={passwordOptions.nbOfCharacters}
+          onChange={handleSlider}
           step={1}
         />
       </div>
@@ -45,32 +100,42 @@ export function PasswordGenerator() {
       <div className="options-container">
         <div className="option">
           <Checkbox
-            name="includeUppercase"
+            name="hasLowerCase"
             onChange={handleCheckBox}
-            checked={options.includeUppercase}
+            checked={passwordOptions.hasLowerCase}
           />
-          <p>Include capital letters</p>
+          <p>Include Lowercase</p>
         </div>
         <div className="option">
           <Checkbox
-            name="includeNumbers"
+            name="hasUpperCase"
             onChange={handleCheckBox}
-            checked={options.includeNumbers}
+            checked={passwordOptions.hasUpperCase}
           />
-          <p>Include numbers</p>
+          <p>Include Upperase</p>
         </div>
         <div className="option">
           <Checkbox
-            name="includeSpecialChars"
+            name="hasNumbers"
             onChange={handleCheckBox}
-            checked={options.includeSpecialChars}
+            checked={passwordOptions.hasNumbers}
+          />
+          <p>Include Numbers</p>
+        </div>
+        <div className="option">
+          <Checkbox
+            name="hasSpecialCharacters"
+            onChange={handleCheckBox}
+            checked={passwordOptions.hasSpecialCharacters}
           />
           <p>Include special characters</p>
         </div>
       </div>
 
       <div className="button-container">
-        <button type="button">Generate password!</button>
+        <button type="button" onClick={handleGenerateClick}>
+          Generate password!
+        </button>
       </div>
     </div>
   );
