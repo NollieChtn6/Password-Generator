09a001a58c96d527b011eb6c709f7ce021e0fe71
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-10-21 20:35:11 +0200

    import PasswordGenerator

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 
-import { PasswordGenerator } from "./assets/PasswordGenerator";
+import { PasswordGenerator } from "./components/PasswordGenerator";
 
 function App() {
   return (
