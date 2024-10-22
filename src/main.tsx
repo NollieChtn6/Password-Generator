f857335c034da7a272bc60bfa76ba402a827fcb6
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-10-22 07:18:42 +0200

    update stylesheet import

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./reset.css";
 import "./index.css";
-import "primereact/resources/themes/lara-light-cyan/theme.css";
+import "primereact/resources/themes/mdc-dark-deeppurple/theme.css";
 
 // https://www.codingdeft.com/posts/react-18-typescript-error/
 const rootElement = document.getElementById("root");
