7437211f224089a509dd0a48cb23ee1df7be97db
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-10-24 21:22:43 +0200

    improve password generation security by using crypto.getRandom() instead of Math.random()

--- src/utils/passwordUtils.ts
+++ src/utils/passwordUtils.ts
@@ -27,6 +27,39 @@ export const generatePassword = ({
   return newPassword;
 };
 
+export const generateSecurePassword = ({
+  nbOfCharacters,
+  hasUpperCase,
+  hasLowerCase,
+  hasNumbers,
+  hasSpecialCharacters,
+}: PasswordOptions): string => {
+  const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const numberChars = "0123456789";
+  const symbolChars = "!@#$%^&*()_+[]{}|;:,.<>?";
+
+  let selectedCharacters = "";
+  if (hasLowerCase) selectedCharacters += lowercaseChars;
+  if (hasUpperCase) selectedCharacters += uppercaseChars;
+  if (hasNumbers) selectedCharacters += numberChars;
+  if (hasSpecialCharacters) selectedCharacters += symbolChars;
+
+  if (!selectedCharacters) {
+    throw new Error("No character types selected for password generation.");
+  }
+  const randomValues = new Uint32Array(nbOfCharacters);
+  crypto.getRandomValues(randomValues);
+  // console.log(randomValues);
+
+  let password = "";
+  for (let i = 0; i < nbOfCharacters; i++) {
+    const randomIndex = randomValues[i] % selectedCharacters.length;
+    password += selectedCharacters[randomIndex];
+  }
+  return password;
+};
+
 export const checkPasswordStrength = (password: string): number => {
   let score = 0;
   if (/[a-z]/.test(password)) score++;
